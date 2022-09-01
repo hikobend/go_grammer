8be3578365cfd2df61733b0c54fbf29582c1601b
hikobend <medamayaki2806@gmail.com>
2022-09-01 22:15:41 +0900

    文字列表示

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
+// fmt : 文法を呼び出す。
+
 import (
 	"fmt"
 )
 
-// fmt : 文法を呼び出す。
-
 func main() {
-	fmt.Println("hello world")
+	// 変数の設定
+	say := "こんにちは"
+
+	// 文字列の中に変数を埋め込む場合、Printf
+	// 変数を埋め込みたい場所に%v.埋め込みたい変数を, sayのように記入
+	// 改行 : \n
+	fmt.Println("say world")
+	fmt.Printf("%v world\n", say)
+	fmt.Printf("%v k!\n", say)
 }
