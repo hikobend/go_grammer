91968f7e304dc2c01cec0e0f8496c316297c479b
hikobend <medamayaki2806@gmail.com>
2022-09-04 15:55:54 +0900

    インターフェース

---
+++ grammer3/main3-9.go
@@ -0,0 +1,33 @@
+// インターフェース
+// 難しい！！1
+
+package main
+
+import (
+	"fmt"
+)
+
+type greeter interface {
+	greet()
+}
+
+type japanese struct {
+}
+
+type american struct {
+}
+
+func (j japanese) greet() {
+	fmt.Println("こんにちは")
+}
+
+func (a american) greet() {
+	fmt.Println("Hello")
+}
+
+func main() {
+	greeters := []greeter{japanese{}, american{}}
+	for _, greeter := range greeters {
+		greeter.greet()
+	}
+}
