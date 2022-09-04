483cd2ad60e672474b6a8c097c1279a2a233d155
hikobend <medamayaki2806@gmail.com>
2022-09-04 20:56:31 +0900

    文字列柄

---
+++ grammer4/main4-6.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// 文字列型
+
+func main() {
+	var s string = "Hello Golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	fmt.Println(`test
+	test
+		test`)
+}
