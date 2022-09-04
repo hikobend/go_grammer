87b23a1e4af62f9d21c8c2b75a5ad438de4d8dad
hikobend <medamayaki2806@gmail.com>
2022-09-04 14:21:20 +0900

    構造体作成

--- grammer3/main3-7.go
+++ grammer3/main3-7.go
@@ -1,9 +1,31 @@
+// 構造体
+
 package main
 
 import (
 	"fmt"
 )
 
+type user struct {
+	name string
+	age  int
+}
+
 func main() {
-	fmt.Println()
+	// ポインタが帰ってくる
+	u := new(user)
+
+	// ポインタが蹴ってこない
+	u.name = "kohei"
+
+	// ポインタが蹴ってこない
+	u.age = 24
+
+	v := user{"dat", 100}
+
+	w := user{name: "friend", age: 99}
+
+	fmt.Println(u)
+	fmt.Println(v)
+	fmt.Println(w)
 }
