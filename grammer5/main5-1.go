595837314bd215803a61f149b91a337b069e5aff
hikobend <hiko.bkend@gmail.com>
2022-09-04 22:52:34 +0900

    定数

--- grammer5/main5-1.go
+++ grammer5/main5-1.go
@@ -1,1 +1,37 @@
-test
+package main
+
+import "fmt"
+
+// 定数
+
+const Pi = 3.14
+
+const (
+	URL      = "http://xxx.co.jp"
+	SiteName = "xxx"
+)
+
+const (
+	A = 1
+	B
+	C
+	D = "A"
+	E
+	F
+)
+
+const (
+	c0 = iota
+	c1
+	c2
+)
+
+func main() {
+	fmt.Println(Pi)
+
+	fmt.Println(URL, SiteName)
+
+	fmt.Println(A, B, C, D, E, F)
+
+	fmt.Println(c0, c1, c2)
+}
