463e0f156ebe685d1587999bfe9d60cb713ed4a3
hikobend <hiko.bkend@gmail.com>
2022-09-05 09:29:50 +0900

    ジェネレーター

---
+++ grammer6/main6-6.go
@@ -0,0 +1,26 @@
+package main
+
+// ジェネレータの実装
+
+import "fmt"
+
+func integers() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	ints := integers()
+
+	fmt.Println(ints())
+	fmt.Println(ints())
+	fmt.Println(ints())
+
+	otherints := integers()
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+	fmt.Println(otherints())
+}
