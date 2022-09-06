5788978d646eae332b2e412bf499a3fc9b820b40
hikobend <hiko.bkend@gmail.com>
2022-09-06 09:23:27 +0900

    ゴルーチン

---
+++ grammer8/main8-9.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// チャネルとゴルーチン
+
+func reciver(c chan int) {
+	for {
+		i := <-c
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	// fmt.Println(<-ch1)
+
+	go reciver(ch1)
+	go reciver(ch2)
+
+	i := 0
+	for i < 100 {
+		ch1 <- i
+		ch2 <- i
+		time.Sleep(50 * time.Millisecond)
+		i++
+	}
+}
