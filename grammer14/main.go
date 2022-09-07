110434d33ff86522fe63a0a794e44cf7f9d3c138
hikobend <hiko.bkend@gmail.com>
2022-09-08 08:29:41 +0900

    Config log設定

---
+++ grammer14/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/m/grammer14/config"
+)
+
+func main() {
+	fmt.Println(config.Config.Port)
+	fmt.Println(config.Config.SQLDriver)
+	fmt.Println(config.Config.DbName)
+	fmt.Println(config.Config.LogFile)
+
+	log.Println("test")
+}
